2a9a092e6bc3b3568c6947c9ba2615a533c9289c
PinkSwuezze <businesseliott@gmail.com>
2024-12-19 15:08:26 -0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,86 @@
+html {
+    background-color:rgba(0, 0, 0, 0.877) !important;
+}
+
+h1 {
+    color:crimson !important;
+    align-content: center;
+    text-align: center;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    background-color: aliceblue;
+    margin-left: 40%;
+    margin-right:40%;
+    margin-top:5%;
+    margin-bottom: 5%;;
+    border-radius: 49%;
+}
+
+.crash {
+    color: firebrick;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    text-align: center;
+    
+}
+.crash2 {
+    color: firebrick;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    text-align: center;
+    font-size: larger;
+    font-style: italic;
+    font-weight: bolder;    
+}
+.crash3{
+    color: firebrick;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    text-align: center;
+    font-size: larger;
+    font-style: italic;
+    font-weight: bolder;    
+}
+.bold {
+    color: rgba(203, 203, 203, 0.74);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: larger;
+    font-style: italic;
+    font-weight: bolder;
+    text-align: center;
+}
+.bold2 {
+    color: rgba(203, 203, 203, 0.74);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: larger;
+    font-style: italic;
+    font-weight: bolder;
+    text-align: center;
+}
+
+.button {
+    background-color: brown;
+    color: antiquewhite;
+    margin-left: 46%;
+    text-align: 20px;
+}
+.button2 {
+    background-color: brown;
+    color: antiquewhite;
+    margin-left: 47%;
+    text-align: 20px;
+}
+.button3 {
+    background-color: brown;
+    color: antiquewhite;
+    margin-left: 48%;
+    text-align: 20px;
+}
+.button4 {
+    background-color: brown;
+    color: antiquewhite;
+    margin-left: 46%;
+    text-align: 20px;
+}
+.button5.onclick:hover {
+    background-color: rgb(0, 255, 76);
+    color: antiquewhite;
+    margin-left: 46%;
+    text-align: 20px;
+}
